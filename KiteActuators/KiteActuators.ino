f7cc7394e7bb8609c2cdf2b52cbf56573996c007
ZakLeonard <79014029+ZakLeonard@users.noreply.github.com>
2021-06-03 09:50:12 -0400

    Add files via upload
    New code to show Chris - KN

---
+++ KiteActuators/KiteActuators.ino
@@ -0,0 +1,106 @@
+#include <SoftwareSerial.h>
+//#include <AltSoftSerial.h>
+#include "ESP32Servo.h"
+#include <stdint.h>
+#include <HardwareSerial.h>
+//AltSoftSerial mySerial;
+//SoftwareSerial mySerial(16, 17); //RX and TX pins on the Nano
+
+HardwareSerial mySerial(1); //RX, TX
+
+Servo servo_starboard;
+Servo servo_port;
+Servo servo_elevator;
+Servo servo_rudder;
+
+uint8_t arr[6];
+
+int starCMD = 1500;
+int portCMD = 1500;
+int elevatorCMD = 1500;
+int rudderCMD = 1500;
+//
+//int starCMDmin = 1334;
+//int starCMDmax = 1666;
+//int portCMDmin = 1334;
+//int portCMDmax = 1666;
+//int elevCMDmin = 1072;
+//int elevCMDmax = 2072;
+//int rudderCMDmin = 1334;
+//int rudderCMDmax = 1666;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  mySerial.begin(115200);
+
+  servo_starboard.attach(14,1334,1666); //14 32 12 15 for new kite. 14 12 32 15 for old kite. - SPER
+  servo_port.attach(32,1334,1666);
+  servo_elevator.attach(12,1072,2072);
+  servo_rudder.attach(15,1334,1666);
+
+  servo_starboard.writeMicroseconds(starCMD);
+  servo_port.write(portCMD);
+  servo_elevator.write(elevatorCMD);
+  servo_rudder.write(rudderCMD);
+
+  //Serial.println("Beginning code.");
+
+  // Delay before running loop()
+  delay(1000);
+
+}
+
+void loop() {
+  while (mySerial.available() < 8)  {
+    delay(1);
+  }
+
+  while (mySerial.read() != 0x01 || mySerial.read() != 0x11)  {
+    while (mySerial.available() < 8) {
+      delay(1);
+    }
+  }
+
+  for (int i = 0; i < 6; i++) {
+    arr[i] = mySerial.read();
+  }
+  uint8_t checksumRecB1 = mySerial.read();
+  uint8_t checksumRecB2 = mySerial.read();  
+  uint16_t checksumRec = ((uint16_t) checksumRecB1 << 8) + (uint16_t) checksumRecB2;
+  uint16_t checksumCalc = checksum(arr,6);
+  Serial.printf("%d, %d\n",checksumRec,checksumCalc);
+
+  if (checksumRec == checksumCalc) {  
+    starCMD = 3000-((arr[1]<<8) + arr[0]);
+    portCMD = starCMD;
+    elevatorCMD = (arr[3]<<8) + arr[2];
+    rudderCMD = (arr[5]<<8) + arr[4]; 
+
+    // For plotting
+    Serial.print(starCMD);
+    Serial.print(", ");
+    Serial.print(elevatorCMD);
+    Serial.print(", ");
+    Serial.println(rudderCMD);
+  
+    servo_starboard.writeMicroseconds(starCMD);
+    servo_port.writeMicroseconds(portCMD);
+    servo_elevator.writeMicroseconds(elevatorCMD);
+    servo_rudder.writeMicroseconds(rudderCMD);
+  }
+}
+
+uint16_t checksum(uint8_t *cmd, uint8_t cmdLen)
+{
+  uint8_t checksum_b1 = 0;
+  uint8_t checksum_b2 = 0;
+  for (int i = 0; i < cmdLen; i++) {
+    checksum_b1 += cmd[i];
+    checksum_b2 += checksum_b1;
+  }
+  return ((uint16_t) checksum_b1 << 8) + (uint16_t) checksum_b2;
+}
---
+++ KiteActuators_outLim/KiteActuators_outLim.ino
@@ -0,0 +1,88 @@
+#include <SoftwareSerial.h>
+//#include <AltSoftSerial.h>
+#include "ESP32Servo.h"
+#include <stdint.h>
+#include <HardwareSerial.h>
+//AltSoftSerial mySerial;
+//SoftwareSerial mySerial(16, 17); //RX and TX pins on the Nano
+
+HardwareSerial mySerial(1); //RX, TX
+
+Servo servo_starboard;
+Servo servo_port;
+Servo servo_elevator;
+Servo servo_rudder;
+
+uint8_t arr[6];
+
+int starCMD = 1500;
+int portCMD = 1500;
+int elevatorCMD = 1500;
+int rudderCMD = 1500;
+
+void setup() {
+
+  Serial.begin(115200);
+
+  pinMode(13, OUTPUT);
+  digitalWrite(13, LOW);
+  mySerial.begin(115200);
+
+  servo_starboard.attach(14,1334,1666); //14 32 12 15 for new kite. 14 12 32 15 for old kite. - SPER
+  servo_port.attach(32,1334,1666);
+  servo_elevator.attach(12,1072,2072);
+  servo_rudder.attach(15,1334,1666);
+
+  servo_starboard.writeMicroseconds(starCMD);
+  servo_port.write(portCMD);
+  servo_elevator.write(elevatorCMD);
+  servo_rudder.write(rudderCMD);
+
+  //Serial.println("Beginning code.");
+
+  // Delay before running loop()
+  //delay(1000);
+
+}
+
+void loop() {
+  while (mySerial.available() < 8)  {
+    delay(1);
+  }
+
+  while (mySerial.read() != 0x01 || mySerial.read() != 0x11)  {
+    while (mySerial.available() < 8) {
+      delay(1);
+    }
+  }
+
+  for (int i = 0; i < 6; i++) {
+    arr[i] = mySerial.read();
+  }
+
+  starCMD = 3000-((arr[1]<<8) + arr[0]);
+  portCMD = starCMD;
+  elevatorCMD = (arr[3]<<8) + arr[2];
+  rudderCMD = (arr[5]<<8) + arr[4]; 
+
+//  limit value of UART signal 
+  starCMD = constrain(starCMD,1334,1666);
+  portCMD = constrain(portCMD,1334,1666);
+  elevatorCMD = constrain(elevatorCMD,1072,2072);
+  rudderCMD = constrain(rudderCMD,1334,1666);
+  
+
+
+//  For plotting
+    Serial.print(starCMD);
+    Serial.print(", ");
+    Serial.print(elevatorCMD);
+    Serial.print(", ");
+    Serial.println(rudderCMD);
+
+
+  servo_starboard.writeMicroseconds(starCMD);
+  servo_port.writeMicroseconds(portCMD);
+  servo_elevator.writeMicroseconds(elevatorCMD);
+  servo_rudder.writeMicroseconds(rudderCMD);
+}
